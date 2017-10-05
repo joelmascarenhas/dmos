442b09e8915ea0b2c3672fd745d6561682356315
osboxes.org <sdjcse1@gmail.com>
2017-10-05 13:55:40 -0400

    queue primitive func

--- Assign2/q.h
+++ Assign2/q.h
@@ -3,7 +3,26 @@
 
 #include "TCB.h"
 
-void InitQ  (TCB_t** q);
-void AddQ   (TCB_t** q,TCB_t** ele);
-void DelQ (TCB_t** q);
-void PrintQ(TCB_t* q);
+void InitQ  (TCB_t* q);
+void AddQ   (TCB_t* q,TCB_t* ele);
+void DelQ   (TCB_t* q);
+void PrintQ (TCB_t* q);
+
+void InitQ(TCB_t* q){
+  q->prev = null;
+  q->next = null;
+}
+
+void AddQ(TCB_t* q,TCB_t* ele){
+  InitQ(ele);
+  TCB_t* runner = q;
+  if(q==NULL){
+    q = ele;
+  }else{
+    while(runner->next!=NULL){
+      runner = runner->next;
+    }
+    runner->next = ele;
+    ele->prev = runner;
+  }
+}
