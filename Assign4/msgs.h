653bdc102c051ab3dbab41a954802a23111959c5
osboxes.org <sdjcse1@gmail.com>
2017-11-16 04:22:17 -0500

    added test code

--- Assign4/msgs.h
+++ Assign4/msgs.h
@@ -5,6 +5,7 @@
 typedef struct port{
   semaphore_t *full,*empty,*accesslock;
   int messageArr[MESSAGES][10];
+  int outPort[MESSAGES];
   int inputPointer,outputPointer;
 }port;
 
@@ -14,10 +15,10 @@ port portObject[PORTS];
 void initPort();
 
 // Used to send message to the intended port
-void send(int intendedPort,int messageSent[]);
+void send(int intendedPort,int messageSent[],int outPort);
 
 // Used to receive message for the intended port
-void receive(int intendedPort, int messageReceived[]);
+void receive(int intendedPort, int messageReceived[], int *outPortR);
 
 
 void initPort(){
@@ -30,7 +31,7 @@ void initPort(){
   }
 }
 
-void send(int intendedPort,int messageSent[]){
+void send(int intendedPort,int messageSent[],int outPort){
   P(portObject[intendedPort].empty);
   P(portObject[intendedPort].accesslock);
 
@@ -39,23 +40,21 @@ void send(int intendedPort,int messageSent[]){
     }
 
     // portObject[intendedPort].messageArr[portObject[intendedPort].inputPointer] = messageSent;
-
-    portObject[intendedPort].in = (portObject[intendedPort].in+1)%MESSAGES;
-
+    portObject[intendedPort].outPort[portObject[intendedPort].inputPointer] = outPort;
+    portObject[intendedPort].inputPointer = (portObject[intendedPort].inputPointer+1)%MESSAGES;
   V(portObject[intendedPort].accesslock);
   V(portObject[intendedPort].full);
 }
 
 
-void receive(int intendedPort, int messageReceived[]){
+void receive(int intendedPort, int messageReceived[],int *outPortR){
   P(portObject[intendedPort].full);
   P(portObject[intendedPort].accesslock);
-
     for(int i=0 ; i < 10 ; i++){
       messageReceived[i]  = portObject[intendedPort].messageArr[portObject[intendedPort].outputPointer][i];
     }
-
-    portObject[port].out = (portObject[port].outputPointer+1)%MESSAGES;
+    *outPortR = portObject[intendedPort].outPort[portObject[intendedPort].outputPointer];
+    portObject[intendedPort].outputPointer = (portObject[intendedPort].outputPointer+1)%MESSAGES;
 
   V(portObject[intendedPort].accesslock);
   V(portObject[intendedPort].empty);
---
+++ Assign4/msgs_test.c
@@ -0,0 +1,73 @@
+#include "msgs.h"
+#define SERVER_PORT 0
+
+int replyPortCounter = 1;
+semaphore_t *replyPortSem;
+
+void server(){
+  int replyPort = 0;
+  int data[10];
+  int dummyPort = 0;
+  printf("Inside Server...\n");
+  while(1){
+    printf("Server receiving data... \n");
+    receive(SERVER_PORT,data,&replyPort);
+    for(int i=0; i<10 ; i++){
+      data[i] = data[i] * 4;
+    }
+    printf("Data after processing in server: ");
+    for(int i=0; i<10 ; i++){
+      printf(" %d ",data[i]);
+    }
+    printf("\n");
+    printf("Server sending data...\n");
+    send(replyPort,data,dummyPort);
+    sleep(1);
+  }
+}
+
+void client(){
+  int tID = currentThreadId();
+  int replyPort = 0;
+  int dummyPort = 0;
+  // Assigning a unique reply port for each client thread
+  P(replyPortSem);
+    replyPort = replyPortCounter++;
+  V(replyPortSem);
+
+  int data[10];
+  for(int i=0 ; i<10 ; i++){
+    data[i] = tID * (i+1);
+  }
+
+  while(1){
+      printf("Data before processing in client %d: ",tID);
+      for(int i=0; i<10 ; i++){
+        printf(" %d ",data[i]);
+      }
+      printf("\n");
+      printf("Client %d sending data...\n",tID);
+      send(SERVER_PORT,data,replyPort);
+      printf("Client %d receiving data...\n",tID);
+      receive(replyPort,data,&dummyPort);
+
+      printf("Data after processing in client %d: ",tID);
+      for(int i=0; i<10 ; i++){
+        printf(" %d ",data[i]);
+      }
+      printf("\n");
+      sleep(1);
+  }
+}
+
+int main(){
+  replyPortSem = CreateSem(1);
+  createQueue();
+  initPort();
+  start_thread(server);
+  start_thread(client);
+  start_thread(client);
+  start_thread(client);
+  run();
+  return 0;
+}
